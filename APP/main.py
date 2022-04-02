7e36caf834c0981b9f783d4c545157a8378afdc9
Mateusz Litwin <m_litwin@wp.pl>
2022-04-02 22:22:53 +0200

    prepare for rendering

--- APP/main.py
+++ APP/main.py
@@ -1,6 +1,6 @@
 from email.policy import default
 from fileinput import filename
-from tkinter import BOTTOM, DISABLED, HORIZONTAL, LEFT, NW, RIGHT, VERTICAL, X, Y, Checkbutton, Label, Spinbox, Tk, font, ttk, Frame, NORMAL, Text, Canvas
+from tkinter import BOTTOM, DISABLED, HORIZONTAL, LEFT, NW, RIGHT, VERTICAL, X, Y, Checkbutton, IntVar, Label, Spinbox, Tk, font, ttk, Frame, NORMAL, Text, Canvas
 from tkinter.constants import TOP, BOTH
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import askyesno
@@ -20,7 +20,7 @@ class App(Tk):
     # ROOT   
     def config_root(self):
         self.resizable(width=False, height=False)
-        self.geometry('1080x800')
+        self.geometry('1080x500')
         self.title("Movie Edit")
         self.main_color = '#eaeaf2'
         self.configure(background = self.main_color)
@@ -80,9 +80,17 @@ class App(Tk):
         
         #RIGHT SIDE     
         #Labels
+        ttk.Label(self, text='INTRO:', style='My.Label').place(x=500, y=10)
+        ttk.Label(self, text='OUTRO:', style='My.Label').place(x=490, y=50)
         ttk.Label(self, text='Tytuł:', style='My.Label').place(x=700, y=10)
         ttk.Label(self, text='Opis:', style='My.Label').place(x=700, y=100)
         ttk.Label(self, text='Miniaturka:', style='My.Label').place(x=700, y=300)
+        
+        #Check Boxes
+        self.intro_var = IntVar(value=1)
+        self.outro_var = IntVar(value=1)
+        self.intro_check = Checkbutton(self, variable=self.intro_var, background='#eaeaf2')
+        self.outro_check = Checkbutton(self, variable=self.outro_var, background='#eaeaf2')
         
         #Entries
         self.titleEntry = ttk.Entry(self, style='TEntry', font=('TkDefaultFont', 14, 'bold'), width=30)
@@ -90,6 +98,8 @@ class App(Tk):
         
         #Button
         self.thBtn = ttk.Button(self, width=15, text='Wybierz...', command=self.button_min_click)
+        self.renderBtn = ttk.Button(self, width=20, text='Render', command=self.render)
+        self.renderPlusBtn = ttk.Button(self, width=20, text='Render i wrzuć', command=self.render)
         
         self.place_widgets()
         
@@ -98,18 +108,22 @@ class App(Tk):
         self.place_left_row(0)
         
         # RIGHT
+        self.intro_check.place(x=600, y=10)
+        self.outro_check.place(x=600, y=50)
         self.titleEntry.insert(0, ' - Dzika Gostomia cz. ***')
         self.titleEntry.place(x=700, y=45)
         self.descriptionEntry.place(x=700, y=135)
         self.thBtn.place(x=900, y=300)
+        self.renderBtn.place(x=600, y=450)
+        self.renderPlusBtn.place(x=800, y=450)
         
     def create_left_row(self):
         row = len(self.left_side_widgets['buttons'])
         self.left_side_widgets['buttons'].append(ttk.Button(self.write_frame, text='Wybierz film', command= lambda i=row: self.button_film_click(i)))
         self.left_side_widgets['from'].append((Spinbox(self.write_frame, from_=0, to=59, justify=RIGHT, width=2, font=('TkDefaultFont', 10)), Spinbox(self.write_frame, from_=0, to=59, width=2, justify=RIGHT, font=('TkDefaultFont', 10))))
         self.left_side_widgets['to'].append((Spinbox(self.write_frame, from_=0, to=59, justify=RIGHT, width=2, font=('TkDefaultFont', 10)), Spinbox(self.write_frame, from_=0, to=59, width=2, justify=RIGHT, font=('TkDefaultFont', 10))))
-        self.left_side_widgets['in'].append(Checkbutton(self.write_frame))
-        self.left_side_widgets['out'].append(Checkbutton(self.write_frame))
+        self.left_side_widgets['in'].append(ttk.Checkbutton(self.write_frame))
+        self.left_side_widgets['out'].append(ttk.Checkbutton(self.write_frame))
         
     def place_left_row(self, row_num):    
         self.left_side_widgets['buttons'][row_num].grid(row = row_num+1, column = 0)
@@ -119,8 +133,8 @@ class App(Tk):
         self.left_side_widgets['to'][row_num][1].grid(row = row_num+1, column=8)
         self.left_side_widgets['in'][row_num].grid(row = row_num+1, column = 11)
         self.left_side_widgets['out'][row_num].grid(row = row_num+1, column = 12)
-        self.left_side_widgets['in'][row_num].select()
-        self.left_side_widgets['out'][row_num].select()
+        #self.left_side_widgets['in'][row_num].select()
+        #self.left_side_widgets['out'][row_num].select()
         
         Label(self.write_frame, text=":", height=3, font=('TkDefaultFont', 14, 'bold')).grid(row = row_num+1, column=2)
         Label(self.write_frame, text=":", height=3, font=('TkDefaultFont', 14, 'bold')).grid(row = row_num+1, column=7)
@@ -141,6 +155,25 @@ class App(Tk):
                 self.left_side_widgets['films'].append(film_name)
                 self.create_left_row()
                 self.place_left_row(row+1)
+    
+    def render(self):
+        out = []
+        for i, film in enumerate(self.left_side_widgets['films']):
+            state_in = self.left_side_widgets['in'][i].state()
+            state_out = self.left_side_widgets['out'][i].state()
+            
+            semi = {}
+            semi['film'] = film
+            semi['from'] = int(self.left_side_widgets['from'][i][0].get()) * 60 + int(self.left_side_widgets['from'][i][1].get())
+            semi['to'] = int(self.left_side_widgets['to'][i][0].get()) * 60 + int(self.left_side_widgets['to'][i][1].get())
+            semi['in'] = 'selected' in state_in or 'alternate' in state_in
+            semi['out'] = 'selected' in state_out or 'alternate' in state_out
+
+            out.append(semi)
+            
+        print(out)
+        print(self.intro_var.get())
+        print(self.outro_var.get())
         
 if __name__ == '__main__':
     App().mainloop()
